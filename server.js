8b9b3c7ac88ab293a3976ddd76eae431a1e80aa1
Esteban <elestebitan@gmail.com>
2026-01-18 19:57:09 -0300

    Preparado para Render

--- server.js
+++ server.js
@@ -4,10 +4,17 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
 
+ HEAD
 // Servir la carpeta "public" (donde estará index.html)
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Ruta raíz
+
+// Servir la carpeta "public"
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta principal
+ 18a7de3 (Preparado para Render)
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
@@ -15,8 +22,18 @@ app.get('/', (req, res) => {
 // Socket.io
 io.on('connection', (socket) => {
   console.log('Usuario conectado');
+<<<<<<< HEAD
   socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
+=======
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Usuario desconectado');
+>>>>>>> 18a7de3 (Preparado para Render)
   });
 });
 
