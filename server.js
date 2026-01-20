dcdd70d6b0b9effce3e0df2945999e53ffb44104
Esteban <elestebitan@gmail.com>
2026-01-20 12:22:05 -0300

    script completo con usuarios

--- server.js
+++ server.js
@@ -8,13 +8,24 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static("public"));
 
 io.on("connection", (socket) => {
-  console.log("Usuario conectado");
 
-  // cuando alguien entra
-  socket.on("usuario conectado", (nombre) => {
-    socket.nombre = nombre;
-    io.emit("mensaje sistema", `🟢 ${nombre} se conectó`);
+  socket.on("join", (username) => {
+    socket.username = username;
+    socket.broadcast.emit("system", `${username} se conectó`);
   });
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    if (socket.username) {
+      socket.broadcast.emit("system", `${socket.username} se desconectó`);
+    }
+  });
+
+});
+
 
   // mensajes normales
   socket.on("chat message", (msg) => {
